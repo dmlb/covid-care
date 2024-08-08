b25b16600788959f5f024ab2c785b2e69f230e75
dmlbs <dmlb@users.noreply.github.com>
2024-08-08 16:39:09 -0600

    add PR tlp, turn on coverage, add tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, UrlTree } from '@angular/router';
+import { Subject } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -35,5 +36,84 @@ describe('AppComponent', () => {
     const link = compiled.querySelector('.skip-to-top-link')
     expect(link).toBeDefined()
     expect(link?.textContent).toContain('Skip to top');
+  });
+
+  it(`should updateCurrentLink on init`, () => {
+    const component = TestBed.createComponent(AppComponent).componentInstance;
+    spyOn(component, 'updateCurrentLink')
+    component.ngOnInit();
+    expect(component.updateCurrentLink).toHaveBeenCalled()
+  });
+
+  it(`should unsubscribe on destroy`, () => {
+    const component = TestBed.createComponent(AppComponent).componentInstance;
+    component.ngOnInit();
+
+    spyOn(component['sub$'], 'unsubscribe')
+    component.ngOnDestroy();
+    expect(component['sub$'].unsubscribe).toHaveBeenCalled()
+  });
+});
+
+
+describe('AppComponent Router', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
+  let routerEventsSubject: Subject<any>;
+
+  beforeEach(async () => {
+    routerEventsSubject = new Subject();
+
+    const routerMock = {
+      events: routerEventsSubject.asObservable(),
+      url: '/initial-url',
+      routerState: {
+        root: {}
+      },
+      createUrlTree: jasmine.createSpy('createUrlTree').and.callFake(() => ({} as UrlTree)),
+      navigateByUrl: jasmine.createSpy('navigateByUrl').and.returnValue(Promise.resolve(true)),
+      navigate: jasmine.createSpy('navigate').and.returnValue(Promise.resolve(true)),
+      serializeUrl: jasmine.createSpy('serializeUrl').and.returnValue('/mock-url'),
+      parseUrl: jasmine.createSpy('parseUrl').and.returnValue({} as UrlTree)
+    };
+
+
+    await TestBed.configureTestingModule({
+      imports: [AppComponent, RouterModule.forRoot([
+        { path: '' },
+        { path: '/new-url', redirectTo: '' },
+        { path: '/old-url', redirectTo: '' }
+      ])],
+      providers: [
+        { provide: Router, useValue: routerMock }
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    router = TestBed.inject(Router);
+  });
+
+  it('should update currentLink on NavigationEnd event', () => {
+
+    component.updateCurrentLink();
+    // default path
+    expect(component.currentLink).toBe('/');
+
+    // Simulate NavigationEnd event
+    const newUrl = '/new-url';
+    (router as any).url = newUrl;  // Type assertion to modify readonly property for testing purposes
+    routerEventsSubject.next(new NavigationEnd(1, '/old-url', newUrl));
+
+
+    expect(component.currentLink).toBe('/new-url'); // Updated to new URL
+  });
+
+  afterEach(() => {
+    if (component['sub$']) {
+      component['sub$'].unsubscribe();
+    }
   });
 });
--- src/app/citations/citations.component.spec.ts
+++ src/app/citations/citations.component.spec.ts
@@ -1,17 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CitationsComponent } from './citations.component';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
+import { ICitation } from '../../interfaces/citations';
+
+const mockCitations: ICitation[] = [
+  { title: 'B Citation', id: '1', sources: [] },
+  { title: 'A Citation', id: '2', sources: [] },
+  { title: 'C Citation', id: '3', sources: [] }
+]
 
 describe('CitationsComponent', () => {
   let component: CitationsComponent;
   let fixture: ComponentFixture<CitationsComponent>;
+  let mockActivatedRoute;
 
   beforeEach(async () => {
+    mockActivatedRoute = {
+      data: of({ })
+    };
+
     await TestBed.configureTestingModule({
-      imports: [CitationsComponent, RouterModule.forRoot([])]
+      imports: [CitationsComponent, RouterModule.forRoot([])],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CitationsComponent);
     component = fixture.componentInstance;
@@ -20,5 +36,40 @@ describe('CitationsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should assign empty array for missing citations on ngOnInit', () => {
+    // Call ngOnInit
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    // Assert that citationData is sorted alphabetically by title
+    expect(component.citationData).toEqual([]);
+  });
+
+  it('should sort citations by title in ngOnInit', () => {
+    mockActivatedRoute = {
+      data: of({ citations: mockCitations })
+    };
+
+    // Replace the existing ActivatedRoute provider with the test-specific mock
+    component['route'] = mockActivatedRoute as any;
+
+    // Call ngOnInit
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    // Assert that citationData is sorted alphabetically by title
+    expect(component.citationData).toEqual([
+      { title: 'A Citation', id: '2', sources: [] },
+      { title: 'B Citation', id: '1', sources: [] },
+      { title: 'C Citation', id: '3', sources: [] }
+    ]);
+  });
+
+  afterEach(() => {
+    if (component['sub$']) {
+      component['sub$'].unsubscribe();
+    }
   });
 });
