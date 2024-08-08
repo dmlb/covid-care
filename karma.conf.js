466ab17812962bb6ce54df5facc35c8c18500cb3
dmlbs <dmlb@users.noreply.github.com>
2024-08-08 17:07:26 -0600

    lcov workflow path

--- karma.conf.js
+++ karma.conf.js
@@ -30,6 +30,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
+        { type: 'json-summary' },
         { type: 'lcov' }
       ]
     },
